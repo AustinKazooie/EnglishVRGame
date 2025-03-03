9c81bcc1ee69c7b9dda53d95f04659d3fc59af94
AustinKazooie <77896489+AustinKazooie@users.noreply.github.com>
2025-03-03 15:53:00 -0500

    Started GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] bool[] activeTargets;
+    [SerializeField] GameObjects[] possibleTargets;
+    [SerializeField] int howManyActiveAtOnce;
+    [SerializeField] int howManyDesiredActive;
+
+    private bool coroutineBool;
+
+    void Update()
+    {
+        howManyAreActive()
+    }
+
+    private int howManyAreActive()
+    {
+        foreach(bool boolean in activeTargets)
+        {
+            if (boolean){howManyActiveAtOnce++}
+        }
+        return howManyDesiredActive - howManyActiveCurrently;
+    }
+
+}
